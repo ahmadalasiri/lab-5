b03b56658927aa05d7efdc435ce9307cc1653bd7
Ahmad Mostafa <124518625+ahmadalasiri@users.noreply.github.com>
2024-10-17 15:17:46 +0300

    Refactor API endpoint responses and update API endpoint URLs for album CRUD operations

--- src/index.js
+++ src/index.js
@@ -30,60 +30,42 @@ app.get("/api/albums", async (req, res) => {
 
 // Get album by title
 app.get("/api/albums/:title", async (req, res) => {
-  try {
-    const album = await Album.find({ title: req.params.title });
-    if (album.length === 0)
-      return res.status(404).json({ message: "Album not found" });
-    res.status(200).json(album);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  const album = await Album.find({ title: req.params.title });
+  if (album.length === 0)
+    return res.status(404).json({ message: "Album not found" });
+  res.status(200).json(album);
 });
 
 // Create a new album
 app.post("/api/albums", async (req, res) => {
   const { title, artist, year } = req.body;
 
-  try {
-    const newAlbum = await Album.create({
-      title,
-      artist,
-      year,
-    });
+  const newAlbum = await Album.create({
+    title,
+    artist,
+    year,
+  });
 
-    res.status(201).json(newAlbum);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+  res.status(201).json(newAlbum);
 });
 
 // Update an album by ID
 app.put("/api/albums/:id", async (req, res) => {
-  try {
-    const updatedAlbum = await Album.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-    if (!updatedAlbum)
-      return res.status(404).json({ message: "Album not found" });
-    res.status(200).json(updatedAlbum);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+  const updatedAlbum = await Album.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!updatedAlbum)
+    return res.status(404).json({ message: "Album not found" });
+  res.status(200).json(updatedAlbum);
 });
 
 // Delete an album by ID
 app.delete("/api/albums/:id", async (req, res) => {
-  try {
-    const album = await Album.findById(req.params.id);
-    if (!album) return res.status(404).json({ message: "Album not found" });
+  const album = await Album.findById(req.params.id);
+  if (!album) return res.status(404).json({ message: "Album not found" });
 
-    await Album.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: "Album deleted successfully" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  await Album.findByIdAndDelete(req.params.id);
+  res.status(200).json({ message: "Album deleted successfully" });
 });
 
 app.get("/api/", (req, res) => {
@@ -93,10 +75,3 @@ app.get("/api/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is up and running on http://localhost:${PORT}`);
 });
-
-// CRUD operations [Create, Read, Update, Delete]
-
-// Create - POST
-// Read - GET
-// Update - PUT
-// Delete - DELETE
